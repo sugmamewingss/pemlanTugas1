8d3d6c0ad79fe6862a2966a3156bc4df48f22972
Alfi Perdiansyah Putra <66344025+sugmamewingss@users.noreply.github.com>
2025-03-10 12:25:02 +0700

    Update MainMobil.java

--- MainMobil.java
+++ MainMobil.java
@@ -14,8 +14,16 @@ public class MainMobil {
         m1.setKecepatan(20); // kecepatan awal mobil, 20km/jam
         m1.setManufaktur("Kijang recing"); // merk awal mobil
         m1.setNoPlat("UI 1231 UA"); // plat awal mobil
-        m1.setWarna("Merah"); // warna awal mobil
+        m1.setWarna("Merah merona"); // warna awal mobil
         m1.setWaktu(1); // waktu yg ditempuh mobil selama perjalanan (satuan jam)
+
+        Mobil m2 = new Mobil();
+        m2.setKecepatan(25); // kecepatan awal mobil, 20km/jam
+        m2.setManufaktur("Civic ban Bocor"); // merk awal mobil
+        m2.setNoPlat("HE 134 T"); // plat awal mobil
+        m2.setWarna("Putih mengkilap"); // warna awal mobil
+        m2.setWaktu(2); // waktu yg ditempuh mobil selama perjalanan (satuan jam)
+
         while (true) { // perulangan, agar program dapat berjalan terus sampai user ingin keluar
             // OPSI MENU
             System.out.println("1. Informasi mobil");
@@ -27,6 +35,7 @@ public class MainMobil {
             switch (masukan) {
                 case 1:
                     m1.displayMessage();
+                    m2.displayMessage();
                     break;
                 case 2:
                     System.out.println();
@@ -41,39 +50,81 @@ public class MainMobil {
                     // SWTICH-CASE LAGI UNTUK OPSI MENU
                     switch (masukan2) {
                         case 1:
+                            System.out.println("Mau ubah mobil 1 apa mobil 2? (Ketik '1' atau '2')");
+                            System.out.print("System: ");
+                            int hebat1 = input.nextInt();
                             System.out.println("Mau ubah jadi merk apa?");
                             System.out.print("System: ");
                             String masukan3 = input.next();
-                            m1.setManufaktur(masukan3); // memanggil ethod void (behavior) di kelas mobil untuk
-                                                        // mengganti merk mobil berdasarkan masukan user
+                            if (hebat1 == 1) {
+                                m1.setManufaktur(masukan3); // memanggil ethod void (behavior) di kelas mobil untuk
+                                // mengganti merk mobil berdasarkan masukan user
+                            } else {
+                                m2.setManufaktur(masukan3); // memanggil ethod void (behavior) di kelas mobil untuk
+                                // mengganti merk mobil berdasarkan masukan user
+                            }
                             break;
                         case 2:
+                            System.out.println("Mau ubah mobil 1 apa mobil 2? (Ketik '1' atau '2')");
+                            System.out.print("System: ");
+                            int hebat2 = input.nextInt();
                             System.out.println("Berapa nomor plat mobilnya?");
                             System.out.print("System: ");
                             String masukan5 = input.next();
-                            m1.setNoPlat(masukan5); // memanggil method void (behavior) di kelas mobil untuk merubah
-                                                    // plat mobil berdasarkan masukan user
+                            if (hebat2 == 1) {
+                                m1.setNoPlat(masukan5); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                // plat mobil berdasarkan masukan user
+                            } else {
+                                m2.setNoPlat(masukan5); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                // plat mobil berdasarkan masukan user
+                            }
                             break;
                         case 3:
+                            System.out.println("Mau ubah mobil 1 apa mobil 2? (Ketik '1' atau '2')");
+                            System.out.print("System: ");
+                            int hebat3 = input.nextInt();
                             System.out.println("Mau warna apa nih mobilnya?");
                             System.out.print("System: ");
                             String masukan6 = input.next();
-                            m1.setWarna(masukan6); // memanggil method void (behavior) di kelas mobil untuk merubah
-                                                   // warna mobil berdasasrkan masukan user
+                            if (hebat3 == 1) {
+                                m1.setWarna(masukan6); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                // warna mobil berdasasrkan masukan user
+                            } else {
+                                m2.setWarna(masukan6); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                // warna mobil berdasasrkan masukan user
+                            }
                             break;
                         case 4:
+                            System.out.println("Mau ubah mobil 1 apa mobil 2? (Ketik '1' atau '2')");
+                            System.out.print("System: ");
+                            int hebat4 = input.nextInt();
                             System.out.println("Berapa kecepatan yang ingin kamu atur? (km/jam)");
                             System.out.print("System: ");
                             double masukan7 = input.nextDouble();
-                            m1.setKecepatan(masukan7); // memanggil method void (behavior) di kelas mobil untuk merubah
-                                                       // kecepatan mobil berdasasrkan masukan user
+                            if (hebat4 == 1) {
+                                m1.setKecepatan(masukan7); // memanggil method void (behavior) di kelas mobil untuk
+                                                           // merubah
+                                // kecepatan mobil berdasasrkan masukan user
+                            } else {
+                                m2.setKecepatan(masukan7); // memanggil method void (behavior) di kelas mobil untuk
+                                                           // merubah
+                                                           // kecepatan mobil berdasasrkan masukan user
+                            }
                             break;
                         case 5:
+                            System.out.println("Mau ubah mobil 1 apa mobil 2? (Ketik '1' atau '2')");
+                            System.out.print("System: ");
+                            int hebat5 = input.nextInt();
                             System.out.println("Berapa lama waktu yang ingin ditempuh mobil? (Satuan jam)");
                             System.out.print("System: ");
                             double masukan4 = input.nextDouble();
-                            m1.setWaktu(masukan4); // memanggil method void (behavior) di kelas mobil untuk merubah
-                                                   // waktu tempuh mobil berdasasrkan masukan user
+                            if (hebat5 == 1) {
+                                m1.setWaktu(masukan4); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                                       // waktu tempuh mobil berdasasrkan masukan user
+                            } else {
+                                m2.setWaktu(masukan4); // memanggil method void (behavior) di kelas mobil untuk merubah
+                                                       // waktu tempuh mobil berdasasrkan masukan user
+                            }
                             break;
                         default:
                             System.out.println("System: Maaf, menu tidak tersedia"); // jika user memasukkan opsi selain
@@ -82,8 +133,8 @@ public class MainMobil {
                     }
                     System.out.println();
                     System.out.println("SIP! Berikut beberapa perubahan dari mobil kamu");
-                    System.out.println();
                     m1.displayMessage();
+                    m2.displayMessage();
                     break;
 
                 case 3:
